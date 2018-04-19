fea48839c071ab27e12a80a86be5ca5e73702198
Ji Yim <gsooyim@gmail.com>
2018-04-19 17:46:21 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {}
+      :matriach => {}
+      :hero => {}
+      :hero_friends = []
       }, 
     :capulet => {
       #your key/value pairs here!

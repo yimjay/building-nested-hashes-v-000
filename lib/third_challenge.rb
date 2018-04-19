242f148f32995c32fed6cd320ecae460122a6ef2
Ji Yim <gsooyim@gmail.com>
2018-04-19 17:50:23 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,11 +6,13 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-        :name => "Lady Montague"
-        
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    }, 
